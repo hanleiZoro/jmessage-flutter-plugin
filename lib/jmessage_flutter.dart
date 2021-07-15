a49d3712cb7e5b723a06ff70536f2f8cef331116
韩雷 <hanlei9257@163.com>
2021-07-15 17:47:33 +0800

    fix: level 变更为可选类型

--- lib/jmessage_flutter.dart
+++ lib/jmessage_flutter.dart
@@ -2436,7 +2436,7 @@ class JMGroupInfo {
   String id; // 群组 id
   String? name; // 群组名称
   String desc; // 群组描述
-  int level; // 群组等级，默认等级 4
+  int? level; // 群组等级，默认等级 4
   String owner; // 群主的 username
   String ownerAppKey; // 群主的 appKey
   int maxMemberCount; // 最大成员数
